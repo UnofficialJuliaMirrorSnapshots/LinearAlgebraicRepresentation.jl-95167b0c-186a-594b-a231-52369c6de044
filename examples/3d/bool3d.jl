8bad2b5c8e49950b3cfccaea985d0369df519618
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-08-07 14:05:36 +0000

    Snapshot of branch master taken on 2019-08-07T10:05:29.856-04:00 from "https://github.com/UnofficialJuliaMirror/LinearAlgebraicRepresentation.jl-95167b0c-186a-594b-a231-52369c6de044"

---
+++ examples/3d/bool3d.jl
@@ -0,0 +1,17 @@
+using LinearAlgebraicRepresentation, ViewerGL, SparseArrays
+Lar = LinearAlgebraicRepresentation; GL = ViewerGL
+
+# 3D Boolean example generation
+#-------------------------------------------------------------------------------
+n,m,p = 1,1,1
+V,(VV,EV,FV,CV) = Lar.cuboidGrid([n,m,p],true)
+cube = V,FV,EV
+
+threecubes = Lar.Struct([ cube,
+    Lar.t(.3,.4,.25), Lar.r(pi/5,0,0), Lar.r(0,0,pi/12), cube,
+    Lar.t(-.2,.4,-.2), Lar.r(0,pi/5,0), Lar.r(0,pi/12,0), cube ])
+
+V,FV,EV = Lar.struct2lar(threecubes)
+GL.VIEW([ GL.GLGrid(V,FV), GL.GLFrame ]);
+
+booleanmatrix = Lar.booleanops(threecubes)
